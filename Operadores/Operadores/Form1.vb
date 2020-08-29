834143f89d83e4882bbff64f8c0621ac65c6c500
Gabriel Quiroz <gabrielquirozdev@gmail.com>
2020-08-29 12:23:31 -0400

    Operadores

---
+++ Operadores/Operadores/Form1.vb
@@ -0,0 +1,12 @@
+﻿Public Class Form1
+    Private Sub Form1_Load(sender As Object, e As EventArgs) Handles MyBase.Load
+        Dim num1, num2, operacion As Double
+        num1 = 2
+        num2 = 5
+        operacion = 5 Mod num2
+
+        num1 -= 5
+
+        MsgBox(num1.ToString)
+    End Sub
+End Class
