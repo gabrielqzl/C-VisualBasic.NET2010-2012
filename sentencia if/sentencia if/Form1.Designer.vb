5bd8a83060ea0d0c5dbd4d3efa235281d48ecde0
Gabriel Quiroz <gabrielquirozdev@gmail.com>
2020-09-02 11:23:39 -0400

    Sentencia If

---
+++ sentencia if/sentencia if/Form1.Designer.vb
@@ -0,0 +1,72 @@
+﻿<Global.Microsoft.VisualBasic.CompilerServices.DesignerGenerated()> _
+Partial Class Form1
+    Inherits System.Windows.Forms.Form
+
+    'Form reemplaza a Dispose para limpiar la lista de componentes.
+    <System.Diagnostics.DebuggerNonUserCode()> _
+    Protected Overrides Sub Dispose(ByVal disposing As Boolean)
+        Try
+            If disposing AndAlso components IsNot Nothing Then
+                components.Dispose()
+            End If
+        Finally
+            MyBase.Dispose(disposing)
+        End Try
+    End Sub
+
+    'Requerido por el Diseñador de Windows Forms
+    Private components As System.ComponentModel.IContainer
+
+    'NOTA: el Diseñador de Windows Forms necesita el siguiente procedimiento
+    'Se puede modificar usando el Diseñador de Windows Forms.  
+    'No lo modifique con el editor de código.
+    <System.Diagnostics.DebuggerStepThrough()> _
+    Private Sub InitializeComponent()
+        Me.txtNum1 = New System.Windows.Forms.TextBox()
+        Me.txtNum2 = New System.Windows.Forms.TextBox()
+        Me.consultar = New System.Windows.Forms.Button()
+        Me.SuspendLayout()
+        '
+        'txtNum1
+        '
+        Me.txtNum1.Location = New System.Drawing.Point(36, 46)
+        Me.txtNum1.Name = "txtNum1"
+        Me.txtNum1.Size = New System.Drawing.Size(100, 20)
+        Me.txtNum1.TabIndex = 0
+        '
+        'txtNum2
+        '
+        Me.txtNum2.Location = New System.Drawing.Point(142, 46)
+        Me.txtNum2.Name = "txtNum2"
+        Me.txtNum2.Size = New System.Drawing.Size(100, 20)
+        Me.txtNum2.TabIndex = 1
+        '
+        'consultar
+        '
+        Me.consultar.Location = New System.Drawing.Point(98, 72)
+        Me.consultar.Name = "consultar"
+        Me.consultar.Size = New System.Drawing.Size(75, 23)
+        Me.consultar.TabIndex = 2
+        Me.consultar.Text = "Consultar"
+        Me.consultar.UseVisualStyleBackColor = True
+        '
+        'Form1
+        '
+        Me.AutoScaleDimensions = New System.Drawing.SizeF(6.0!, 13.0!)
+        Me.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font
+        Me.ClientSize = New System.Drawing.Size(282, 143)
+        Me.Controls.Add(Me.consultar)
+        Me.Controls.Add(Me.txtNum2)
+        Me.Controls.Add(Me.txtNum1)
+        Me.Name = "Form1"
+        Me.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen
+        Me.Text = "If"
+        Me.ResumeLayout(False)
+        Me.PerformLayout()
+
+    End Sub
+
+    Friend WithEvents txtNum1 As TextBox
+    Friend WithEvents txtNum2 As TextBox
+    Friend WithEvents consultar As Button
+End Class
---
+++ sentencia if/sentencia if/Form1.vb
@@ -0,0 +1,23 @@
+﻿Public Class Form1
+    Private Sub consultar_Click(sender As Object, e As EventArgs) Handles consultar.Click
+        'Dim num1, num2 As Integer
+
+        'num1 = txtNum1.Text
+        'num2 = txtNum2.Text
+
+        'If (num1 < num2) Then
+        '    MsgBox("Numero 1 menor que numero 2")
+        'ElseIf (num1 = num2) Then
+        '    MsgBox("Numero 1 es igual que numero 2")
+        'Else
+        '    MsgBox("Numero 1 es mayor que numero 2")
+        'End If
+
+        If (txtNum1.Text.Contains("facilito")) Then
+            MsgBox("La frase contiene la palabra facilito")
+        Else
+            MsgBox("No contiene la palabra facilito")
+        End If
+
+    End Sub
+End Class
