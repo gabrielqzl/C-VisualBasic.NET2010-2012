d35d9e1ec31f7b25fe771b7acf923f95ce2151f5
Gabriel Quiroz <gabrielquirozdev@gmail.com>
2020-09-02 09:55:13 -0400

    Botones
    Practica con Botones en Visual Basic

---
+++ Botones/Botones/Form1.Designer.vb
@@ -0,0 +1,109 @@
+﻿<Global.Microsoft.VisualBasic.CompilerServices.DesignerGenerated()>
+Partial Class Form1
+    Inherits System.Windows.Forms.Form
+
+    'Form reemplaza a Dispose para limpiar la lista de componentes.
+    <System.Diagnostics.DebuggerNonUserCode()>
+    Protected Overrides Sub Dispose(ByVal disposing As Boolean)
+        Try
+            If disposing AndAlso components IsNot Nothing Then
+                components.Dispose()
+            End If
+        Finally
+            MyBase.Dispose(disposing)
+        End Try
+    End Sub
+
+    'Requerido por el Diseñador de Windows Forms
+    Private components As System.ComponentModel.IContainer
+
+    'NOTA: el Diseñador de Windows Forms necesita el siguiente procedimiento
+    'Se puede modificar usando el Diseñador de Windows Forms.  
+    'No lo modifique con el editor de código.
+    <System.Diagnostics.DebuggerStepThrough()>
+    Private Sub InitializeComponent()
+        Me.BtnSuma = New System.Windows.Forms.Button()
+        Me.TxtNum1 = New System.Windows.Forms.TextBox()
+        Me.TxtNum2 = New System.Windows.Forms.TextBox()
+        Me.lblNum1 = New System.Windows.Forms.Label()
+        Me.lblNum2 = New System.Windows.Forms.Label()
+        Me.btnCerrar = New System.Windows.Forms.Button()
+        Me.SuspendLayout()
+        '
+        'BtnSuma
+        '
+        Me.BtnSuma.Location = New System.Drawing.Point(12, 71)
+        Me.BtnSuma.Name = "BtnSuma"
+        Me.BtnSuma.Size = New System.Drawing.Size(146, 39)
+        Me.BtnSuma.TabIndex = 0
+        Me.BtnSuma.Text = "Sumar"
+        Me.BtnSuma.UseVisualStyleBackColor = True
+        '
+        'TxtNum1
+        '
+        Me.TxtNum1.Location = New System.Drawing.Point(12, 42)
+        Me.TxtNum1.Name = "TxtNum1"
+        Me.TxtNum1.Size = New System.Drawing.Size(70, 20)
+        Me.TxtNum1.TabIndex = 1
+        '
+        'TxtNum2
+        '
+        Me.TxtNum2.Location = New System.Drawing.Point(88, 42)
+        Me.TxtNum2.Name = "TxtNum2"
+        Me.TxtNum2.Size = New System.Drawing.Size(70, 20)
+        Me.TxtNum2.TabIndex = 2
+        '
+        'lblNum1
+        '
+        Me.lblNum1.AutoSize = True
+        Me.lblNum1.Location = New System.Drawing.Point(12, 26)
+        Me.lblNum1.Name = "lblNum1"
+        Me.lblNum1.Size = New System.Drawing.Size(53, 13)
+        Me.lblNum1.TabIndex = 3
+        Me.lblNum1.Text = "Número 1"
+        '
+        'lblNum2
+        '
+        Me.lblNum2.AutoSize = True
+        Me.lblNum2.Location = New System.Drawing.Point(85, 26)
+        Me.lblNum2.Name = "lblNum2"
+        Me.lblNum2.Size = New System.Drawing.Size(53, 13)
+        Me.lblNum2.TabIndex = 4
+        Me.lblNum2.Text = "Número 2"
+        '
+        'btnCerrar
+        '
+        Me.btnCerrar.Location = New System.Drawing.Point(12, 116)
+        Me.btnCerrar.Name = "btnCerrar"
+        Me.btnCerrar.Size = New System.Drawing.Size(146, 39)
+        Me.btnCerrar.TabIndex = 5
+        Me.btnCerrar.Text = "Cerrar"
+        Me.btnCerrar.UseVisualStyleBackColor = True
+        '
+        'Form1
+        '
+        Me.AutoScaleDimensions = New System.Drawing.SizeF(6.0!, 13.0!)
+        Me.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font
+        Me.ClientSize = New System.Drawing.Size(170, 169)
+        Me.Controls.Add(Me.btnCerrar)
+        Me.Controls.Add(Me.lblNum2)
+        Me.Controls.Add(Me.lblNum1)
+        Me.Controls.Add(Me.TxtNum2)
+        Me.Controls.Add(Me.TxtNum1)
+        Me.Controls.Add(Me.BtnSuma)
+        Me.FormBorderStyle = System.Windows.Forms.FormBorderStyle.SizableToolWindow
+        Me.Name = "Form1"
+        Me.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen
+        Me.Text = "Calculadora"
+        Me.ResumeLayout(False)
+        Me.PerformLayout()
+
+    End Sub
+
+    Friend WithEvents BtnSuma As Button
+    Friend WithEvents TxtNum1 As TextBox
+    Friend WithEvents TxtNum2 As TextBox
+    Friend WithEvents lblNum1 As Label
+    Friend WithEvents lblNum2 As Label
+    Friend WithEvents btnCerrar As Button
+End Class
---
+++ Botones/Botones/Form1.vb
@@ -0,0 +1,15 @@
+﻿Public Class Form1
+    Private num1, num2, suma As Integer
+
+    Private Sub BtnUno_Click(sender As Object, e As EventArgs) Handles BtnSuma.Click
+        num1 = TxtNum1.Text
+        num2 = TxtNum2.Text
+        suma = num1 + num2
+        MsgBox(suma, MsgBoxStyle.Information, "Resultado de Suma")
+
+    End Sub
+
+    Private Sub btnCerrar_Click(sender As Object, e As EventArgs) Handles btnCerrar.Click
+        Me.Close()
+    End Sub
+End Class
