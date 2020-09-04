574044c1ddfebda82a7e8cb949e0b714879574ed
Gabriel Quiroz <gabrielquirozdev@gmail.com>
2020-09-04 15:04:11 -0400

    Función para recibir solo números

---
+++ soloNumeros/soloNumeros/Form1.vb
@@ -0,0 +1,23 @@
+﻿Public Class Form1
+    Private Sub Form1_Load(sender As Object, e As EventArgs) Handles MyBase.Load
+
+    End Sub
+
+    Public Sub soloNumeros(ByRef e As System.Windows.Forms.KeyPressEventArgs)
+        If Char.IsDigit(e.KeyChar) Then
+            e.Handled = False
+        ElseIf Char.IsControl(e.KeyChar) Then
+            e.Handled = False
+        Else
+            e.Handled = True
+        End If
+
+        MsgBox(e.KeyChar.ToString)
+
+
+    End Sub
+
+    Private Sub TextBox1_KeyPress(sender As Object, e As KeyPressEventArgs) Handles TextBox1.KeyPress
+        soloNumeros(e)
+    End Sub
+End Class
