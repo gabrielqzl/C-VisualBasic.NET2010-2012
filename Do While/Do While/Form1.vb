e081f6cf2485a56242eb06b474f017c140dc1028
Gabriel Quiroz <gabrielquirozdev@gmail.com>
2020-09-04 09:20:19 -0400

    Ciclo Do While

---
+++ Do While/Do While/Form1.vb
@@ -0,0 +1,44 @@
+﻿Public Class Form1
+    Private Sub Form1_Load(sender As Object, e As EventArgs) Handles MyBase.Load
+        'Dim i As Integer = 0
+
+        'Do
+        '    MsgBox("Ciclo Do = " + i.ToString)
+        '    i += 1
+        'Loop While i < 3
+
+        'i = 0
+
+        'Do While i < 3
+        '    MsgBox("Ciclo Do While = " + i.ToString)
+        '    i = i + 1
+        'Loop
+
+
+        'i = 0
+
+        'While i < 3
+        '    MsgBox("Ciclo While = " + i.ToString)
+        '    i += 1
+        'End While
+
+        Dim i As Integer = 0
+        Dim arreglo(5) As Integer
+
+        Do While i < 5
+            arreglo(i) = i
+            i += 1
+        Loop
+
+        i = 0
+
+        While i < 5
+            MsgBox("Arreglo(" + i.ToString + ")= " + arreglo(i).ToString)
+            i += 1
+        End While
+
+
+
+    End Sub
+
+End Class
---
+++ Do While/Do While/My Project/AssemblyInfo.vb
@@ -0,0 +1,35 @@
+﻿Imports System
+Imports System.Reflection
+Imports System.Runtime.InteropServices
+
+' La información general de un ensamblado se controla mediante el siguiente 
+' conjunto de atributos. Cambie estos valores de atributo para modificar la información
+' asociada con un ensamblado.
+
+' Revisar los valores de los atributos del ensamblado
+
+<Assembly: AssemblyTitle("Do While")>
+<Assembly: AssemblyDescription("")>
+<Assembly: AssemblyCompany("")>
+<Assembly: AssemblyProduct("Do While")>
+<Assembly: AssemblyCopyright("Copyright ©  2020")>
+<Assembly: AssemblyTrademark("")>
+
+<Assembly: ComVisible(False)>
+
+'El siguiente GUID sirve como id. de typelib si este proyecto se expone a COM.
+<Assembly: Guid("c425c3ff-917c-43cc-a791-5179ab76b81b")>
+
+' La información de versión de un ensamblado consta de los cuatro valores siguientes:
+'
+'      Versión principal
+'      Versión secundaria
+'      Número de compilación
+'      Revisión
+'
+' Puede especificar todos los valores o usar los valores predeterminados de número de compilación y de revisión
+' utilizando el carácter "*", como se muestra a continuación:
+' <Assembly: AssemblyVersion("1.0.*")>
+
+<Assembly: AssemblyVersion("1.0.0.0")>
+<Assembly: AssemblyFileVersion("1.0.0.0")>
