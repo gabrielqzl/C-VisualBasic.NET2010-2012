3f7f975ab99a854ef3da94d64e806c48d397ee66
Gabriel Quiroz <gabrielquirozdev@gmail.com>
2020-09-04 12:19:13 -0400

    Funciones con Parametros

---
+++ FuncionesConParametros/FuncionesConParametros/Form1.vb
@@ -0,0 +1,29 @@
+﻿Public Class Form1
+    Private Sub Form1_Load(sender As Object, e As EventArgs) Handles MyBase.Load
+        Dim var As String
+        var = "Variables sin cambiar"
+
+
+        porReferencia(var)
+
+        MsgBox(var)
+
+
+    End Sub
+
+    Private Sub porValor(ByVal variable As String)
+        MsgBox(variable)
+        variable = "Variable cambiada"
+
+        MsgBox(variable)
+    End Sub
+
+
+    Private Sub porReferencia(ByRef variable As String)
+        MsgBox(variable)
+        variable = "Variable cambiada"
+
+        MsgBox(variable)
+    End Sub
+
+End Class
---
+++ FuncionesConParametros/FuncionesConParametros/My Project/AssemblyInfo.vb
@@ -0,0 +1,35 @@
+﻿Imports System
+Imports System.Reflection
+Imports System.Runtime.InteropServices
+
+' La información general de un ensamblado se controla mediante el siguiente 
+' conjunto de atributos. Cambie estos valores de atributo para modificar la información
+' asociada con un ensamblado.
+
+' Revisar los valores de los atributos del ensamblado
+
+<Assembly: AssemblyTitle("FuncionesConParametros")>
+<Assembly: AssemblyDescription("")>
+<Assembly: AssemblyCompany("")>
+<Assembly: AssemblyProduct("FuncionesConParametros")>
+<Assembly: AssemblyCopyright("Copyright ©  2020")>
+<Assembly: AssemblyTrademark("")>
+
+<Assembly: ComVisible(False)>
+
+'El siguiente GUID sirve como id. de typelib si este proyecto se expone a COM.
+<Assembly: Guid("c2ca237b-6ad7-4db8-a9b6-0ea523c6898b")>
+
+' La información de versión de un ensamblado consta de los cuatro valores siguientes:
+'
+'      Versión principal
+'      Versión secundaria
+'      Número de compilación
+'      Revisión
+'
+' Puede especificar todos los valores o usar los valores predeterminados de número de compilación y de revisión
+' utilizando el carácter "*", como se muestra a continuación:
+' <Assembly: AssemblyVersion("1.0.*")>
+
+<Assembly: AssemblyVersion("1.0.0.0")>
+<Assembly: AssemblyFileVersion("1.0.0.0")>
