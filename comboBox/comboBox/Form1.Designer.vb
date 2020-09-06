ec6784079238b66c8c8e6abadf35723ac7c34656
Gabriel Quiroz <gabrielquirozdev@gmail.com>
2020-09-06 10:53:10 -0400

    Llenado de comboBox de forma manual

---
+++ comboBox/comboBox/Form1.Designer.vb
@@ -0,0 +1,64 @@
+﻿<Global.Microsoft.VisualBasic.CompilerServices.DesignerGenerated()> _
+Partial Class Form1
+    Inherits System.Windows.Forms.Form
+
+    'Form reemplaza a Dispose para limpiar la lista de componentes.
+    <System.Diagnostics.DebuggerNonUserCode()> _
+    Protected Overrides Sub Dispose(ByVal disposing As Boolean)
+        Try
+            If disposing AndAlso components IsNot Nothing Then
+                components.Dispose()
+            End If
+        Finally
+            MyBase.Dispose(disposing)
+        End Try
+    End Sub
+
+    'Requerido por el Diseñador de Windows Forms
+    Private components As System.ComponentModel.IContainer
+
+    'NOTA: el Diseñador de Windows Forms necesita el siguiente procedimiento
+    'Se puede modificar usando el Diseñador de Windows Forms.  
+    'No lo modifique con el editor de código.
+    <System.Diagnostics.DebuggerStepThrough()> _
+    Private Sub InitializeComponent()
+        Me.cmbGenero = New System.Windows.Forms.ComboBox()
+        Me.BackgroundWorker1 = New System.ComponentModel.BackgroundWorker()
+        Me.Button1 = New System.Windows.Forms.Button()
+        Me.SuspendLayout()
+        '
+        'cmbGenero
+        '
+        Me.cmbGenero.FormattingEnabled = True
+        Me.cmbGenero.Location = New System.Drawing.Point(12, 44)
+        Me.cmbGenero.Name = "cmbGenero"
+        Me.cmbGenero.Size = New System.Drawing.Size(121, 21)
+        Me.cmbGenero.TabIndex = 0
+        '
+        'Button1
+        '
+        Me.Button1.Location = New System.Drawing.Point(139, 42)
+        Me.Button1.Name = "Button1"
+        Me.Button1.Size = New System.Drawing.Size(75, 23)
+        Me.Button1.TabIndex = 1
+        Me.Button1.Text = "Button1"
+        Me.Button1.UseVisualStyleBackColor = True
+        '
+        'Form1
+        '
+        Me.AutoScaleDimensions = New System.Drawing.SizeF(6.0!, 13.0!)
+        Me.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font
+        Me.ClientSize = New System.Drawing.Size(386, 319)
+        Me.Controls.Add(Me.Button1)
+        Me.Controls.Add(Me.cmbGenero)
+        Me.Name = "Form1"
+        Me.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen
+        Me.Text = "Form1"
+        Me.ResumeLayout(False)
+
+    End Sub
+
+    Friend WithEvents cmbGenero As Windows.Forms.ComboBox
+    Friend WithEvents BackgroundWorker1 As System.ComponentModel.BackgroundWorker
+    Friend WithEvents Button1 As Button
+End Class
---
+++ comboBox/comboBox/Form1.vb
@@ -0,0 +1,32 @@
+﻿Public Class Form1
+    Private Sub Form1_Load(sender As Object, e As EventArgs) Handles MyBase.Load
+
+        Dim dt As DataTable = New DataTable("tabla")
+
+        dt.Columns.Add("Codigo")
+        dt.Columns.Add("Descripcion")
+
+        Dim dr As DataRow
+
+        dr = dt.NewRow()
+
+        dr("Codigo") = 0
+        dr("Descripcion") = "Masculino"
+        dt.Rows.Add(dr)
+
+        dr = dt.NewRow()
+
+        dr("Codigo") = 1
+        dr("Descripcion") = "Femenino"
+        dt.Rows.Add(dr)
+
+        cmbGenero.DataSource = dt
+        cmbGenero.ValueMember = "Codigo"
+        cmbGenero.DisplayMember = "Descripcion"
+
+    End Sub
+
+    Private Sub Button1_Click(sender As Object, e As EventArgs) Handles Button1.Click
+        MsgBox(cmbGenero.SelectedValue.ToString)
+    End Sub
+End Class
