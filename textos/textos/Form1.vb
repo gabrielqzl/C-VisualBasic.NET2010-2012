911a497ca228e6f8216385eb6f501676df507259
Gabriel Quiroz <gabrielquirozdev@gmail.com>
2020-09-06 10:30:29 -0400

    Comparar String

---
+++ textos/textos/Form1.vb
@@ -0,0 +1,23 @@
+﻿Public Class Form1
+    Private Sub Form1_Load(sender As Object, e As EventArgs) Handles MyBase.Load
+        'Dim cadena As String
+        'cadena = "Hola Mundo "
+
+        'Dim verifica As Boolean
+        'verifica = cadena.Contains("facilito")
+
+        'If verifica = True Then
+        '    MsgBox("La cadena si contiene la palabra facilito")
+        'Else
+        '    MsgBox("La cadena no contiene la palabra facilito")
+        'End If
+
+        Dim comparacion As Integer
+
+        comparacion = StrComp("codigo", "Codigo")
+        MsgBox(comparacion.ToString)
+
+
+
+    End Sub
+End Class
